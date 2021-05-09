3ce34c6cfe3a6b7d29460e9b23f2ab90824bdf6e
John <johnb9682@gmail.com>
2021-05-09 13:34:18 +0800

    Init Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "Soccer Manager",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tpcc-soccer-manager-app
 React Native Mobile Application 
+
+## How to start application in local
+```
+git clone git@github.com:johnb9682/tpcc-soccer-manager-app.git
+npm install
+npm start
+```
