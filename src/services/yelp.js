16ea37b90304ecfa36607c903240948c3409b153
Song Yu <song0180@e.ntu.edu.sg>
2021-05-09 17:07:09 +0800

    Update server ip address

--- src/services/yelp.js
+++ src/services/yelp.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export default Axios.create(
     {
-        baseURL: 'http://13.250.52.217:8080',
+        baseURL: 'http://3.138.135.180',
         headers: {
         }
     }
