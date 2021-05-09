7efacb75b27f141e87212d263062f8d08f171149
John <johnb9682@gmail.com>
2021-05-09 15:57:56 +0800

    Add API Call

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
 import { Text, StyleSheet } from "react-native";
+import yelp from "../services/yelp";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+
+  const [restaurant, setRestaurant] = useState({});
+
+  const getRestaurantDetailById = async () => {
+      var response = await yelp.get(`/test`, {});
+      if (response) {
+          console.info(response.data);
+          setRestaurant(response.data);
+      }
+      console.info("getRestaurantDetailById");
+      console.info(response);
+  };
+
+  useEffect(() => {
+      getRestaurantDetailById();
+      console.info("useEffect");
+  }, []);
+
+  return <Text style={styles.text}>{restaurant}</Text>;
 };
 
 const styles = StyleSheet.create({
---
+++ src/services/yelp.js
@@ -0,0 +1,10 @@
+import Axios from 'axios';
+
+export default Axios.create(
+    {
+        baseURL: 'http://13.250.52.217:8080',
+        headers: {
+        }
+    }
+
+);
