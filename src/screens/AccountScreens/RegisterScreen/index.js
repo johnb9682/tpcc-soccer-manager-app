be983814a296345990eccf2b69a9767ef1a8408d
NicedeEric <ericding0110@gmail.com>
2021-07-19 17:05:03 +0800

    Add Register Screen Feature

--- src/screens/AccountScreens/RegisterScreen/index.js
+++ src/screens/AccountScreens/RegisterScreen/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-import { Heading } from '../../../components';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import { styles } from './style';
+import { Button, Heading, Input, Loading } from '../../../components';
+import { useAuthStore } from '../../../shared/zustand/auth';
 
 const Register = ({ navigation }) => {
+  const { isLoading } = useAuthStore();
+  const [emailText, setEmailText] = React.useState('');
+  const [passwordText, setPasswordText] = React.useState('');
+
+  const onSignUpPress = React.useCallback(() => {
+    return <Text>Sign Up Successful</Text>
+    // login(emailText, passwordText);
+  });
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <View>
-      <Heading>Register Screen Placeholder</Heading>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.container}
+        keyboardDismissMode="on-drag"
+      >
+        <Heading containerStyle={styles.heading}>Sign Up</Heading>
+        <View>
+          <Input placeholder="Enter your e-mail" onChangeText={setEmailText} />
+          <Input
+            placeholder="Enter your password"
+            onChangeText={setPasswordText}
+            secureTextEntry={true}
+          />
+          <Input
+            placeholder="Repeat your password"
+            onChangeText={setPasswordText}
+            secureTextEntry={true}
+          />
+          <Button
+            title="Confirm to Sign Up"
+            onPress={onSignUpPress}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
