7da7800369e3d2b9de013dd87f82c01d672ca979
song0180 <song0180@e.ntu.edu.sg>
2021-07-25 02:50:12 +0800

    add simple email validation

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,6 +1,4 @@
 export const isValidEmail = emailStr => {
   const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-  const result = reg.test(emailStr);
-  console.log(result);
-  return result;
+  return reg.test(emailStr);
 };
