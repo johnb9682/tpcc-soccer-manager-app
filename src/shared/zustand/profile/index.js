949c5883d2655d682f5a195d09c87ac77fec9941
NicedeEric <ericding0110@gmail.com>
2021-08-21 13:08:12 +0800

    Add State for profilStore

--- src/shared/zustand/profile/index.js
+++ src/shared/zustand/profile/index.js
@@ -1,29 +1,65 @@
 import create from 'zustand';
-
-import { mockUser } from './mockData';
-
+import {updateProfile, getUserInfo} from '../../api/profile'
+import { clearCache } from '../../api/cache';
+import { storeDataObj, getDataObj, removeDataObj } from '../../utils/asyncStorage';
+import {USER_INFO_SESSION_STORAGE_FIELD} from '../../api/auth/constants'
+import { useAuthStore } from '../auth';
 const initialState = {
     isLoading: false,
-    userEmail: "ericding0110@gmail.com",
-    userName: "NicedeEric",
 };
 
 export const useProfileStore = create((set, get) => ({
     ...initialState,
     fetchUserInfo: async userId => {
-        set({ isLoading: true });
-        // call fetchUserInfo api using userId
-        // get userName, userEmail and avatarUri
-        set({
-            userEmail: mockUser['email'],
-            userName: mockUser['username'],
-        });
-        set({ isLoading: false });
+        try {
+            const response = await getUserInfo(userId);
+            const status = response.status;
+            const data = response.data;
+            return data;
+        }
+        catch (error) {
+            throw new Error(error.message);
+        }
     },
-    updateUseInfo: async (userId, userEmail, userName) => {
-        set({
-            userEmail: userEmail,
-            userName: userName,
-        })
+    updateUseInfo: async (email, userId, userName) => {
+        const result = await updateProfile(email, userId, userName, "123456");
+        const status = result.status;
+        const data = result.data;
+        switch (status) {
+            case 200: {
+                    const updatedUserInfo = {
+                        email: data.email,
+                        userName: data.userName,
+                        userId: userId,
+                    };
+                    // await removeDataObj(USER_INFO_SESSION_STORAGE_FIELD);
+                    // await clearCache();
+                    await storeDataObj(USER_INFO_SESSION_STORAGE_FIELD, updatedUserInfo);
+                return {
+                    status: 'success',
+                    message: 'You have successfully changed your profile information!',
+                    data: {
+                        userName,
+                    },
+                };
+            }
+            case 401: // FALLTHROUGH 
+            case 403:
+                return {
+                    status: 'info',
+                    message: 'You are not allowed to change your profile. Please contact admin.',
+                    data: {
+                    // Empty
+                    },
+                };
+            default:
+                return {
+                    status: 'error',
+                    message:'404 Not Found',
+                    data: {
+                    // Empty
+                    },
+                };
+        }
     },
 }))
