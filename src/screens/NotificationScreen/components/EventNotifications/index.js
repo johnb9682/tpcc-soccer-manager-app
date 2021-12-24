c7728d472762e6968dd5e2b8a501202c566aa86a
song0180 <song0180@e.ntu.edu.sg>
2021-12-24 16:00:30 +0800

    revamp event notifications

--- src/screens/NotificationScreen/components/EventNotifications/index.js
+++ src/screens/NotificationScreen/components/EventNotifications/index.js
@@ -117,6 +117,58 @@ const EventNotifications = ({
         >
           Invitations Sent
         </Heading>
+        {invitationsSent.map((eventInvitation) => {
+          return (
+            <View style={styles.eventItem} key={eventInvitation.invitationId}>
+              <View style={styles.header}>
+                <View style={styles.infoPair}>
+                  <Icon
+                    name='calendar-clock'
+                    size={18}
+                    color={THEME_COLORS.BLACK}
+                    style={styles.icon}
+                  />
+                  <Text numberOfLines={1} style={styles.title}>
+                    {eventInvitation.eventName}
+                  </Text>
+                </View>
+                <View style={styles.infoPair}>
+                  <Text>Status:</Text>
+                </View>
+              </View>
+              <View style={styles.body}>
+                <View style={styles.infoPair}>
+                  <Icon
+                    name='clock-time-four-outline'
+                    size={14}
+                    color={THEME_COLORS.DEFAULT_BLUE_PRIMARY}
+                    style={styles.icon}
+                  />
+                  <Text
+                    numberOfLines={1}
+                    style={[
+                      styles.timeText,
+                      { color: THEME_COLORS.DEFAULT_BLUE_PRIMARY },
+                    ]}
+                  >
+                    {dayjs(eventInvitation.createTime).format(dateFormat)}
+                  </Text>
+                </View>
+                <View style={styles.infoPair}>
+                  <Icon
+                    name='account'
+                    size={12}
+                    color={THEME_COLORS.DEFAULT_INFO_TEXT}
+                    style={styles.icon}
+                  />
+                  <Text numberOfLines={1} style={styles.textValue}>
+                    Receiver: {eventInvitation.receiverName}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          );
+        })}
         {!invitationsSent.length && (
           <RoundRectContainer minHeight={50}>
             <NoData message='No Event Invitations Sent' />
