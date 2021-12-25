43ea843efc03298042c142b2941eaad99f63bda4
Marco Ye <marcomufeng@gmail.com>
2021-12-25 16:49:49 +0800

    edited team notification screen

--- src/shared/zustand/invitation/index.js
+++ src/shared/zustand/invitation/index.js
@@ -7,6 +7,13 @@ import {
   deleteEventInvitation,
   addEventInvitation,
   respondEventInvitation,
+
+  getTeamInvitation,
+  getUserReceiverTeamInvitation,
+  getUserSenderTeamInvitation,
+  deleteTeamInvitation,
+  addTeamInvitation,
+  respondTeamInvitation,
 } from '../../api/invitation';
 
 const initialState = {
@@ -82,6 +89,80 @@ export const useInvitationStore = create((set, get) => ({
   respondEventInvitation: async (invitationId, respondValue) => {
     set({ isLoading: true });
     const result = await respondEventInvitation(invitationId, respondValue);
+    set({ isLoading: false });
+    if (typeof result === 'string') {
+      return { type: 'error', message: result };
+    } else {
+      return {
+        type: 'success',
+        message: `You have responded the invitation!`,
+      };
+    }
+  },
+
+  fetchTeamInvitationInfo: async (invitationId) => {
+    set({ isLoading: true });
+    const result = await getTeamInvitation(invitationId);
+    set({ isLoading: false });
+    if (typeof result === 'string') {
+      return { type: 'error', message: result };
+    } else {
+      return {
+        type: 'success',
+        message: `Successfully fetched the info 
+          of team invitation of ID: ${invitationId}`,
+      };
+    }
+  },
+  fetchUserTeamInvitation: async (userId) => {
+    set({ isLoading: true });
+    const result1 = await getUserReceiverTeamInvitation(userId);
+    const result2 = await getUserSenderTeamInvitation(userId);
+    set({ isLoading: false });
+    if (typeof result1 === 'string') {
+      return { type: 'error', message: result1 };
+    } else if (typeof result2 === 'string') {
+      return { type: 'error', message: result2 };
+    } else {
+      set({
+        userTeamInvitationsReceived: result1.data.invitationTeamResponses,
+        userTeamInvitationsSent: result2.data.invitationTeamResponses,
+      });
+      return {
+        type: 'success',
+        message: 'Successfully fetched all team invitations!',
+      };
+    }
+  },
+  deleteTeamInvitation: async (invitationId) => {
+    set({ isLoading: true });
+    const result = await deleteTeamInvitation(invitationId);
+    set({ isLoading: false });
+    if (typeof result === 'string') {
+      return { type: 'error', message: result };
+    } else {
+      return {
+        type: 'success',
+        message: `Successfully deleted team invitation! (ID: ${invitationId})`,
+      };
+    }
+  },
+  addTeamInvitation: async (teamId, receiverIds, senderId) => {
+    set({ isLoading: true });
+    const result = await addTeamInvitation(teamId, receiverIds, senderId);
+    set({ isLoading: false });
+    if (typeof result === 'string') {
+      return { type: 'error', message: result };
+    } else {
+      return {
+        type: 'success',
+        message: `Successfully sent a team invitation!`,
+      };
+    }
+  },
+  respondTeamInvitation: async (invitationId, respondValue) => {
+    set({ isLoading: true });
+    const result = await respondTeamInvitation(invitationId, respondValue);
     set({ isLoading: false });
     if (typeof result === 'string') {
       return { type: 'error', message: result };
